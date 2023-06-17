82109fa1dabbac01e81e58be0fc9fdce4463e044
sidee <said.issack@student.moringaschool.com>
2023-06-17 13:46:30 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    return {
+      ...employee,
+      [key]: undefined
+    }; }
+
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  }
+
